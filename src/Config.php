2503b46896389994606c34bc0bf0b1e9c3dc30ba
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 12:04:34 +0100

    Added custom config factory.

---
+++ src/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Title: iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvanced_Config extends Pronamic_WP_Pay_Gateways_IDealAdvanced_Config {
+	public function get_payment_server_url() {
+		return 'https://www.ideal-simulator.nl/professional/';
+	}
+
+	public function get_certificates() {
+		return array(
+			dirname( __FILE__ ) . '/../certificates/simulator.cer',
+		);
+	}
+}
--- src/Integration.php
+++ src/Integration.php
@@ -1,9 +1,21 @@
 <?php
 
+/**
+ * Title: iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvanced_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'ideal-simulator-ideal-advanced';
 		$this->name       = 'iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw';
 		$this->provider   = 'ideal-simulator';
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvanced_ConfigFactory';
 	}
 }
