862509b65d8edfd4887f75609ccf65d1b7940553
Remco Tolsma <info@remcotolsma.nl>
2015-12-29 11:20:37 +0100

    Added tests.

---
+++ tests/ConfigTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Title: iDEAL Simulator - iDEAL Professional / Advanced / Zelfbouw  - Config test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvanced_ConfigTest extends PHPUnit_Framework_TestCase {
+	public function test_config() {
+		$config = new Pronamic_WP_Pay_Gateways_IDealSimulator_IDealAdvanced_Config();
+
+		$expected = 'https://www.ideal-simulator.nl/professional/';
+
+		$url = $config->get_payment_server_url();
+
+		$this->assertEquals( $expected, $url );
+	}
+}
